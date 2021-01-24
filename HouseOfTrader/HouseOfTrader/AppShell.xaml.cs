f11e750e33bde1e359ea84ad31e540dc04468f06
Onkar Murkar <onkar.murkar@otiselevator.com>
2021-01-24 23:13:02 +0530

    Upload the Base Project Files

---
+++ HouseOfTrader/HouseOfTrader/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using HouseOfTrader.ViewModels;
+using HouseOfTrader.Views;
+using Xamarin.Forms;
+
+namespace HouseOfTrader
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
